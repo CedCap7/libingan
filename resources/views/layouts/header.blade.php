3005c7573062591ec90539b1a8349f7f12d01b55
D.G. <150104602+dgguayan@users.noreply.github.com>
2025-05-03 12:58:58 +0800

    header clickable when phone size

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -31,6 +31,9 @@
     </div>
   </div>
 </nav>
+
+<!-- Add Bootstrap JS at the end of body for navbar toggle functionality -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
   
 <div class="">
 @yield('content')
